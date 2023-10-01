92be2207b8d8279908d6f0958a59774af1ad8a06
RichardTham1 <tham-richard@hotmail.com>
2023-10-01 19:48:37 -0400

    RT: initialize project

---
+++ src/main/java/com/example/javaserv/JavaservApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaserv/JavaservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
