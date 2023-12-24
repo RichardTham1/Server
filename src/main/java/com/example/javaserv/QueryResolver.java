5b5f84bb4df0bf0b89a53141ecfacbeb4ebe2975
RichardTham1 <tham-richard@hotmail.com>
2023-12-24 14:53:26 -0500

    Managed to make the first version of graphql work

--- src/main/java/com/example/javaserv/QueryResolver.java
+++ src/main/java/com/example/javaserv/QueryResolver.java
@@ -1,12 +1,23 @@
 package com.example.javaserv;
 
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Controller
 public class QueryResolver {
-    @SchemaMapping(typeName = "Query")
-    public User getUserById(String id) {
-        return null;
+    @QueryMapping
+    public User getUser(@Argument String email) {
+        return User.getUser(email);
     }
+
+    @QueryMapping
+    public List<Course> courses(User user) {
+        return user.getCourses();
+    }
+
 }
