bd71eae86bad0c1c843c8a58b8ce42b13c4696a5
RichardTham1 <tham-richard@hotmail.com>
2023-12-23 20:42:19 -0500

    added course hashmap to user

--- src/main/java/com/example/javaserv/User.java
+++ src/main/java/com/example/javaserv/User.java
@@ -1,8 +1,11 @@
 package com.example.javaserv;
 
 
+import java.util.HashMap;
+
 public class User {
     private String uid;
+    private HashMap<String, Course> courses;
     public User(String uid) { this.uid = uid; }
 
 }
