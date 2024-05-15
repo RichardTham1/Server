bb2750a888255730b785f18a8bbff52fed4d0c77
RichardT15 <71038343+RichardT15@users.noreply.github.com>
2024-05-15 19:26:14 -0400

    Update addcourse parameter

--- Java/src/main/java/com/example/javaserv/User.java
+++ Java/src/main/java/com/example/javaserv/User.java
@@ -5,21 +5,31 @@ import java.util.List;
 import java.util.Objects;
 
 public class User {
-    private String email;
-    private List<Course> courses = new ArrayList<>();
+    private final String email;
+    private long lastActivity;
+    private final List<Course> courses = new ArrayList<>();
     public User(String email) {
         this.email = email;
+        this.lastActivity = System.currentTimeMillis();
     }
 
     public String getEmail() {
         return this.email;
     }
 
-    public void addCourse(String courseName) {
-        courses.add(new Course(courseName));
+    public void addCourse(Course course) {
+        courses.add(course);
     }
 
     public List<Course> getCourses() {
         return courses;
+    }
+
+    public long getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setLastActivity(long lastActivity) {
+        this.lastActivity = lastActivity;
     }
 }
