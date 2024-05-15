554e778d4690fb64b4d29f5d61cdbbf5491bf5d0
RichardT15 <71038343+RichardT15@users.noreply.github.com>
2024-05-15 19:24:09 -0400

    Added userservice to mutation resolver

--- Java/src/main/java/com/example/javaserv/MutationResolver.java
+++ Java/src/main/java/com/example/javaserv/MutationResolver.java
@@ -1,26 +1,33 @@
 package com.example.javaserv;
 
 import com.example.javaserv.service.FirestoreService;
+import com.example.javaserv.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class MutationResolver {
     @Autowired
     private FirestoreService firestoreService;
+
+    @Autowired
+    private UserService userService;
+
     @MutationMapping
     public User createUser(@Argument String email) {
         User newUser = new User(email);
         firestoreService.addUser(newUser);
+        userService.createUser(newUser);
         return newUser;
     }
 
-    /*@MutationMapping
-    public User addCourse(@Argument String email, @Argument String courseName) {
-        User user = User.getUser(email);
-        user.addCourse(courseName);
+    @MutationMapping
+    public User addCourse(@Argument String email, @Argument Course course) {
+        User user = userService.getUser(email);
+        user.addCourse(course);
         return user;
-    }*/
+    }
 }
