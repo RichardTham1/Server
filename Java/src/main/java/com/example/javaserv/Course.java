a237fc9a8f892162e8339e60b1c35e24db780cb1
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-09-16 12:30:22 -0400

    Added getter/setter and implemented evaluate

--- Java/src/main/java/com/example/javaserv/Course.java
+++ Java/src/main/java/com/example/javaserv/Course.java
@@ -1,33 +1,82 @@
 package com.example.javaserv;
 
 
+import com.google.cloud.firestore.DocumentSnapshot;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Course {
-    private List<Mark> markList;
+    private ArrayList<Mark> marks;
     private String courseName;
     private Integer passingGrade;
-    private Integer grade = 0;
-    private Integer accumulatedPoints = 0;
-    public Course(String courseName, Integer passingGrade, ArrayList<Mark> marks) {
+    private Float grade = 0f;
+    private Float accumulatedPoints = 0f;
+    private Float pointsLeft;
+
+    public Course() {}
+
+    public void setCourseName(String courseName) {
         this.courseName = courseName;
-        this.passingGrade = passingGrade;
-        this.markList = marks;
     }
 
-    private void evaluateAccumulatedPoints() {
-        accumulatedPoints = 0;
-        for (Mark mark: markList) {
-            accumulatedPoints += mark.getMark() * mark.getWeight();
+    public void setAccumulatedPoints(Float accumulatedPoints) {
+        this.accumulatedPoints = accumulatedPoints;
+    }
+
+    public void setGrade(Float grade) {
+        this.grade = grade;
+    }
+
+    public void setMarks(ArrayList<Mark> marks) {
+        this.marks = marks;
+    }
+
+    public void setPassingGrade(Integer passingGrade) {
+        if (passingGrade > 100) {
+            this.passingGrade = 100;
+        } else {
+            this.passingGrade = passingGrade;
         }
     }
-    
-    private void evaluateGrade() {
-        // TODO: 2023-12-23 verify how to calculate grade for without having all the marks entered 
+
+    public void evaluate() {
+        accumulatedPoints = 0f;
+        Float weightSum = 0f;
+        for (Mark mark: marks) {
+            if (mark.getMark() == null) continue;
+            accumulatedPoints += mark.getMark() * mark.getWeight() / 100;
+            weightSum += mark.getWeight();
+        }
+        grade = Math.round((100 * accumulatedPoints / weightSum) * 10) / 10.0f;
+        pointsLeft = 100f - weightSum;
     }
 
-    public Integer getGrade() {
+    public ArrayList<Mark> getMarks() {
+        return marks;
+    }
+
+    public Float getGrade() {
         return grade;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Integer getPassingGrade() {
+        return passingGrade;
+    }
+
+    public Float getAccumulatedPoints() {
+        return accumulatedPoints;
+    }
+
+    public Float getPointsLeft() {
+        return pointsLeft;
+    }
+
+    public void setPointsLeft(Float pointsLeft) {
+        this.pointsLeft = pointsLeft;
     }
 }
