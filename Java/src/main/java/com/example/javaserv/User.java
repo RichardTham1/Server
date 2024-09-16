6e889391c3dee8218faad61c27a20e43f68f8888
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-09-16 12:32:24 -0400

    added modify course and remove course on user

--- Java/src/main/java/com/example/javaserv/User.java
+++ Java/src/main/java/com/example/javaserv/User.java
@@ -1,18 +1,29 @@
 package com.example.javaserv;
+import com.google.common.collect.Iterables;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
 public class User {
-    private final String email;
+    private String email;
     private long lastActivity;
-    private final List<Course> courses = new ArrayList<>();
+    private final ArrayList<Course> courses = new ArrayList<>();
 
     public User(String email, long lastActivity) {
         this.email = email;
         this.lastActivity = System.currentTimeMillis();
     }
+
+    /*public User(String email, long lastActivity, List<Course> courses) {
+        this.email = email;
+        this.lastActivity = System.currentTimeMillis();
+        this.courses.addAll(courses);
+    }*/
+
+    public User(){}
+
     public User(String email) {
         this.email = email;
         this.lastActivity = System.currentTimeMillis();
@@ -24,6 +35,17 @@ public class User {
 
     public void addCourse(Course course) {
         courses.add(course);
+    }
+
+    public void modifyCourse(String courseName, Course courseToModify) {
+        int index = Iterables.indexOf(courses, course -> course.getCourseName().equals(courseName));
+        courses.remove(index);
+        courses.add(index, courseToModify);
+    }
+
+    public void removeCourse(String courseName) {
+        int index = Iterables.indexOf(courses, course -> course.getCourseName().equals(courseName));
+        courses.remove(index);
     }
 
     public List<Course> getCourses() {
